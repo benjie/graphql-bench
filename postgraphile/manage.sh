255cc76d89944bf3c04409fc5748f74b9398c131
Benjie Gillam <benjie@jemjie.com>
2018-05-16 20:07:09 +0100

    Detect number of processors on OSX too

--- postgraphile/manage.sh
+++ postgraphile/manage.sh
@@ -3,13 +3,21 @@
 set -e
 
 SCRIPT_DIR=$(dirname "$0")
+if hash nproc 2>/dev/null; then
+  NPROC=nproc
+elif hash gnproc 2>/dev/null; then
+  NPROC=gnproc
+else
+  echo "No nproc installed"
+  exit 1
+fi;
 
 usage() {
     echo "Usage: $0 (init|start|nuke)"
 }
 
 init() {
-    N_CPUS=$(nproc --all)
+    N_CPUS=$($NPROC --all)
     docker build --no-cache -t "hasura/postgraphile:latest" "$SCRIPT_DIR"
     docker run --name postgraphile-chinook -p 5000:5000 -d hasura/postgraphile:latest postgraphile -c 'postgres://admin@172.17.0.1:7432/chinook' --host 0.0.0.0 --max-pool-size 100 --cluster-workers "$N_CPUS"
 }
